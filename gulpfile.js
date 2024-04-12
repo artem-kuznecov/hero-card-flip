1dd18521d26c6d71b07dd13f66f9149344377f42
Влад Коваленко <kovalenko@MacBook-Air-Vlad-2.local>
2024-04-12 18:37:16 +0300

    add artem loh

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,34 @@
 const { exec } = require('child_process')
 const gulp = require('gulp')
-
+vl;mlkbfdndvasl;fke'fwmq'lqwmfwl;mewgl;mwegl;geqnlegfwknfweefw
 const convertStyles = () => {
 	exec('npm run sass', (error, stdout, stderr) => {
 		if (error) {
 			console.log(`artem loh`)
 			return
-		}
+		}ewklbaenlkgewnewfgkl;nf2m1;'wvml;vewevvew
 		if (stderr) {
 			console.log(`stderr: ${stderr}`)
-			return
-		}
-	})
-}
+			returnbaer
+		}abre
+	})bare
+}ber
 
-function watchStyles() {
-	gulp.watch('public/style.scss', { ignoreInitial: false }, function (cb) {
-		convertStyles()
-		cb()
-	})
-}
-
-module.exports = { watchStyles }
+funcbewqtion watchStyles() {
+	bwrgulp.watch('public/style.scss', { ignoreInitial: false }, function (cb) {
+	rbe	convertStyles()
+	bfdr	cb()
+	})br
+}ef
+bresb
+modulfrede.exports = { watchStyles }
+bfdbfrd
+b
+ref
+bfd
+bfrde
+brfd
+bre
+ber
+rbf
+bdf
